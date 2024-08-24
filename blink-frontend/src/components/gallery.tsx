6ce211aac8ba42bec31ebb21bfc59bf7dee9c138
Bianca Ren <biancarongg@gmail.com>
2024-08-24 18:39:43 +1000

    efrgeefdssfegbrf

--- blink-frontend/src/components/gallery.tsx
+++ blink-frontend/src/components/gallery.tsx
@@ -1,22 +1,33 @@
 import { Box } from '@mui/material';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
+import ModalImage from "react-modal-image"
+
+const styles = {
+    container: {
+      display: 'flex',        
+      justifyContent: 'center',
+      alignItems: 'center',      
+      margin: 0,              
+    }
+  };
 
 function ImageGallery() {
     const images = require.context('../../public/photo/', false, /\.(png|jpg)$/);
     const imageList = images.keys().map(image => images(image));
-    
+
     return (
         <Box width="100%"  height="100%" style={{display: 'flex',  justifyContent:'center', alignItems:'center'}}>
             <ImageList sx={{ width:"100%", height:"100%", padding:"5%" }} cols={4} gap={20}>
             {imageList.map((item) => (
                 <ImageListItem key={item}>
-                    <img
-                        srcSet={item}
-                        src={item}
-                        alt={item.title}
-                        loading="lazy"
+                    <div style={styles.container}>
+                    <ModalImage
+                        small={item}
+                        large={item}
                     />
+                    </div>
+                    
                 </ImageListItem>
             ))}
             </ImageList>
