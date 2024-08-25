483cc984eb2adc158a2e63d4c9a31de47f9e5ae8
Chris Yoo <chris.yoo@safetyculture.io>
2024-08-25 14:50:43 +1000

    set ig background

--- blink-frontend/src/App.tsx
+++ blink-frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-import { Box, Typography } from '@mui/material';
+import { BottomNavigation, Box, Typography } from '@mui/material';
 import { ThemeProvider, createTheme } from "@mui/material/styles";
 import CssBaseline from "@mui/material/CssBaseline";
 import { grey } from '@mui/material/colors';
@@ -8,13 +8,14 @@ import Carousel from 'react-material-ui-carousel';
 import ImageGallery from './components/gallery';
 import BigPhoto from './components/bigPhoto';
 import ImagesClient from './clients/images-client';
+import BgPhoto from './assets/instagram-colour-background.png';
 
 function App() {
   const theme = createTheme({
     palette: {
-      background: {
-        default: "black"
-      },
+      // background: {
+      //   default: "black"
+      // },
       primary: {
         main: grey[100],
       },
@@ -32,14 +33,27 @@ function App() {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <Box width="100%" flexDirection="column" paddingY="3%">
-        <Box width="100%" justifyContent="center" alignItems="center">
-          <Typography variant="h1" gutterBottom align="center" color="primary">Terrible Ideas Hackathon</Typography>
-          <Typography variant="h4" gutterBottom align="center" color="primary">Check out the photos taken</Typography>
+      <Box>
+        <Box 
+          width="100%"
+          flexDirection="column"
+          paddingY="3%"
+          sx={{
+            backgroundImage:`url(${BgPhoto})`,
+            backgroundRepeat: "no-repeat",
+            backgroundSize: "cover",
+            height: "100%",
+          }}
+        >
+          <Box width="100%" justifyContent="center" alignItems="center">
+            <Typography variant="h1" fontWeight="bold" gutterBottom align="center" color="primary">Terrible Ideas Hackathon</Typography>
+            <Typography variant="h4" gutterBottom align="center" color="primary">Check out the photos taken!</Typography>
+          </Box>
+          <Carousel autoPlay={true}></Carousel>
+          <BigPhoto images={images}/>
+          <ImageGallery images={images} />
         </Box>
-        <Carousel autoPlay={true}></Carousel>
-        <BigPhoto images={images}/>
-        <ImageGallery images={images} />
+        <footer></footer>
       </Box>
     </ThemeProvider>
   );
